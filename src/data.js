3b561367cc212fb900ea0efb6dd07ec4e5cea7cc
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2022-11-01 09:56:14 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,15 +1,20 @@
 const allRecipesData = [
-    { id: 0, name: 'Biscuits', img: 'https://raw.githubusercontent.com/ARBUCHELI/REACT-FAVORITE-RECIPES/master/img/biscuits.jpg'},
-    { id: 1, name: 'Bulgogi', img: 'https://raw.githubusercontent.com/ARBUCHELI/REACT-FAVORITE-RECIPES/master/img/bulgogi.jpg'},
-    { id: 2, name: 'Calamari', img: 'https://raw.githubusercontent.com/ARBUCHELI/REACT-FAVORITE-RECIPES/master/img/calamari.jpg'},
-    { id: 3, name: 'Ceviche', img: 'https://raw.githubusercontent.com/ARBUCHELI/REACT-FAVORITE-RECIPES/master/img/ceviche.jpg'},
-    { id: 4, name: 'Cheeseburger', img: 'https://raw.githubusercontent.com/ARBUCHELI/REACT-FAVORITE-RECIPES/master/img/cheeseburger.jpg'},
-    { id: 5, name: 'Churrasco', img: 'https://raw.githubusercontent.com/ARBUCHELI/REACT-FAVORITE-RECIPES/master/img/churrasco.jpg'},
-    { id: 6, name: 'Dumplings', img: 'https://raw.githubusercontent.com/ARBUCHELI/REACT-FAVORITE-RECIPES/master/img/dumplings.jpg'},
-    { id: 7, name: 'Fish & Chips', img: 'https://raw.githubusercontent.com/ARBUCHELI/REACT-FAVORITE-RECIPES/master/img/fishnchips.jpg'},
-    { id: 8, name: 'Hummus', img: 'https://raw.githubusercontent.com/ARBUCHELI/REACT-FAVORITE-RECIPES/master/img/hummus.jpg'},
-    { id: 9, name: 'Masala Dosa', img: 'https://raw.githubusercontent.com/ARBUCHELI/REACT-FAVORITE-RECIPES/master/img/masaladosa.jpg'},
-    { id: 10, name: 'Pad Thai', img: 'https://raw.githubusercontent.com/ARBUCHELI/REACT-FAVORITE-RECIPES/master/img/padthai.jpg'},
+    { id: 0, name: 'Vulgar Display of Power - Pantera', img: 'https://upload.wikimedia.org/wikipedia/ru/thumb/1/12/PanteraVulgarDisplayofPower.jpg/411px-PanteraVulgarDisplayofPower.jpg'},
+    { id: 1, name: 'Far Beyond Driven - Pantera', img: 'https://upload.wikimedia.org/wikipedia/en/d/d3/Pantera-FarBeyondDriven.jpg'},
+    { id: 2, name: 'Cowboys From Hell - Pantera', img: 'https://upload.wikimedia.org/wikipedia/en/a/a8/CowboysFromHell.jpg?20110108050209'},
+    { id: 3, name: 'The Great Southern Trendkill', img: 'https://upload.wikimedia.org/wikipedia/en/3/30/Pantera_The_Great_Southern_Trendkill.jpg'},
+    { id: 4, name: 'Master of Puppets - Metallica', img: 'https://upload.wikimedia.org/wikipedia/en/b/b2/Metallica_-_Master_of_Puppets_cover.jpg?20110906092130'},
+    { id: 5, name: 'Metallica - Black Album', img: 'https://upload.wikimedia.org/wikipedia/en/2/2c/Metallica_-_Metallica_cover.jpg'},
+    { id: 6, name: 'Ride The Lightning - Metallica', img: 'https://upload.wikimedia.org/wikipedia/en/f/f4/Ridetl.png'},
+    { id: 7, name: "Kill 'Em All - Metallica", img: 'https://upload.wikimedia.org/wikipedia/en/5/5c/Metallica_-_Kill_%27Em_All_cover.jpg'},
+    { id: 8, name: 'And Justice For All - Metallica', img: 'https://upload.wikimedia.org/wikipedia/en/b/bd/Metallica_-_...And_Justice_for_All_cover.jpg'},
+    { id: 9, name: 'Hysteria - Def Leppard', img: 'https://upload.wikimedia.org/wikipedia/en/4/40/Def_Leppard_-_Hysteria_%28vinyl_version%29.jpg'},
+    { id: 10, name: 'Pyromania - Def Leppard', img: 'https://upload.wikimedia.org/wikipedia/en/a/a7/Def_Leppard_-_Pyromania.jpg'},
+    { id: 11, name: 'Vault Greatest Hits - Def Leppard', img: 'https://upload.wikimedia.org/wikipedia/en/4/43/Def_Leppard_-_Vault_Greatest_Hits_%281980%E2%80%931995%29.jpg'},
+    { id: 12, name: 'Killers - Iron Maiden', img: 'https://upload.wikimedia.org/wikipedia/en/b/b4/Iron_Maiden_Killers.jpg'},
+    { id: 13, name: 'Fear of the Dark - Iron Maiden', img: 'https://upload.wikimedia.org/wikipedia/en/e/eb/Iron_Maiden_-_Fear_Of_The_Dark.jpg'},
+    { id: 14, name: 'Somewhere in Time - Iron Maiden', img: 'https://upload.wikimedia.org/wikipedia/en/9/9b/Iron_Maiden_-_Somewhere_in_Time.jpg'},
+    { id: 15, name: 'Piece of Mind - Iron Maiden', img: 'https://upload.wikimedia.org/wikipedia/en/8/85/Iron_Maiden_-_Piece_Of_Mind.jpg'},
   ];
   
-  export default allRecipesData;
+export default allRecipesData;
